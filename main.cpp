866364351a1e50f4a010dc7e085e9d14d311b5f6
Paul Novack <paul.novack@gmail.com>
2022-05-15 08:16:00 -0500

    adds to get id parameter

--- main.cpp
+++ main.cpp
@@ -19,15 +19,19 @@ int main(void) {
     Client cli("boxes:boxes@127.0.0.1/boxes", ClientOption::POOL_MAX_SIZE, 60);
     // use Session sess as usual
     served::multiplexer mux;
-    mux.handle("/boxes")
+    mux.handle("/boxes/{id}")
             .get([&](served::response &res, const served::request & req) {
+                //req.params["id"];
                 std::string name;
                
                 Session sess = cli.getSession();
-                SqlResult myRows = sess.sql("SELECT user_id, name,weight, picture,created_at FROM boxes where user_id = 1").execute();
+                SqlResult myRows = sess.sql("SELECT user_id, name,weight, picture,created_at FROM boxes where user_id = ?")
+                        .bind(req.params["id"]).execute();
                 std::stringstream buffer;
+                bool hasRows = false;
                 buffer << "[";
                 for (Row row : myRows.fetchAll()) {
+                    hasRows = true;
                     buffer << "{";
                     buffer << "\"user_id\":" << "\"" << row[0] << "\",\n";
                     buffer << "\"name\":" << "\"" << row[1] << "\",\n";
@@ -35,7 +39,7 @@ int main(void) {
                     buffer << "\"picture\":" << "\"" << row[3] << "\"\n";
                     buffer << "},";
                 }
-                buffer.seekp(-1, std::ios_base::end);
+                if(hasRows)buffer.seekp(-1, std::ios_base::end);
                 buffer << "]";
                 name = buffer.str();
                 res.set_header("content-type", "application/json");
