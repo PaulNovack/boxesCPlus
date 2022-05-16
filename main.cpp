20bec1a557919a05261276d1d6a9e480da51f2c5
Paul Novack <paul.novack@gmail.com>
2022-05-16 10:40:34 -0500

    testing authkey

--- main.cpp
+++ main.cpp
@@ -2,31 +2,70 @@
 #include <iostream>
 #include <iostream>
 #include <served/served.hpp>
-
-
 #include <mysqlx/xdevapi.h>
-
 using namespace mysqlx;
-
-
-
-
 using namespace std;
 
 
-int main(void) {
+std::mutex l_authkey_map;
+std::map<std::string, int> authKeys;
 
-    Client cli("boxes:boxes@host.docker.internal/boxes", ClientOption::POOL_MAX_SIZE, 60);
+
+
+std::string getAuthKey(std::string cookie){
+    int pos = cookie.find("authToken=");
+     std::string h = "";
+    if(pos != -1){
+        h = cookie.substr(pos+10,cookie.length()-10);
+        return h;    
+    } else {
+        return h;
+    }
+}
+int getUserIdFromAuthKey(Client &cli,std::string authKey){
+    Session sess = cli.getSession();
+     //std::lock_guard<std::mutex> guard(l_authkey_map);
+     int user_id = 0;
+     std::map<std::string, int>::iterator it;
+     it = authKeys.find(authKey);
+     if(it != authKeys.end()){
+       user_id = it->second; 
+       return user_id;
+     } else {
+        SqlResult myRows = sess.sql("SELECT id FROM users where authkey = ?")
+                .bind(authKey).execute();
+        for (Row row : myRows.fetchAll()) {
+            user_id = row[0];
+        }
+        if(user_id != 0){
+            authKeys.insert(std::make_pair(authKey, user_id));
+
+        }
+
+        return user_id;
+     }
+}
+
+
+
+
+int main(void) {
+    Client cli("boxes:boxes@127.0.0.1/boxes", ClientOption::POOL_MAX_SIZE, 60);
     // use Session sess as usual
     served::multiplexer mux;
-    mux.handle("/boxes/{id}")
+    mux.handle("/boxes")
             .get([&](served::response &res, const served::request & req) {
-                //req.params["id"];
                 std::string name;
-               
+                std::string header = req.header("cookie");
+                std::string authKey = getAuthKey(header);
+ 
+     
                 Session sess = cli.getSession();
+                int user_id = getUserIdFromAuthKey(cli,authKey);
+                
+                
                 SqlResult myRows = sess.sql("SELECT user_id, name,weight, picture,created_at FROM boxes where user_id = ?")
-                        .bind(req.params["id"]).execute();
+                        .bind(user_id).execute();
                 std::stringstream buffer;
                 bool hasRows = false;
                 buffer << "[";
@@ -43,6 +82,7 @@ int main(void) {
                 buffer << "]";
                 name = buffer.str();
                 res.set_header("content-type", "application/json");
+                res.set_header("Set-Cookie", "authToken=asdfghjkloiuytrewhkderwthysgedor;SameSite=Strict;Expires=Sat, Oct 15, 2022 00:00:00 GMT");
                 res << name;
             });
             
