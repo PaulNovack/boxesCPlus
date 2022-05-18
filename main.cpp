023479f451c8378e0908c9615ac6e03cd5f09a89
Paul Novack <paul.novack@gmail.com>
2022-05-18 08:49:09 -0500

    scoping changes improve cookie logic

--- main.cpp
+++ main.cpp
@@ -82,7 +82,30 @@ std::string getQueryParam(std::string url, std::string param) {
     return ret;
 }
 
+std::map<std::string, std::string> parseCookies(std::string const& s) {
+    std::map<std::string, std::string> m;
+
+    std::string::size_type key_pos = 0;
+    std::string::size_type key_end;
+    std::string::size_type val_pos;
+    std::string::size_type val_end;
+
+    while ((key_end = s.find('=', key_pos)) != std::string::npos) {
+        if ((val_pos = s.find_first_not_of(";", key_end)) == std::string::npos)
+            break;
+
+        val_end = s.find(';', val_pos);
+        m.emplace(s.substr(key_pos, key_end - key_pos), s.substr(val_pos + 1, val_end - val_pos - 1));
+
+        key_pos = val_end;
+        if (key_pos != std::string::npos)
+            ++key_pos;
+    }
+
+    return m;
+}
 // replace this with a proper cookie parser !
+
 std::string getAuthKey(std::string cookie) {
     int pos = cookie.find("authToken=");
     int delpos = cookie.find(";");
@@ -135,9 +158,15 @@ int main(void) {
     mux.handle("/item/{id}")
 
             .get([&](served::response &res, const served::request & req) {
+                std::string authKey = "";
                 std::string name;
                 std::string header = req.header("cookie");
-                std::string authKey = getAuthKey(header);
+                auto cookies = parseCookies(header);
+                std::map<std::string, std::string>::iterator it;
+                it = cookies.find("authToken");
+                if (it != cookies.end()) {
+                    authKey = it->second;
+                }
                 std::string box_id_str = req.params["id"];
                 int box_id = stoi(box_id_str);
                 Session sess = cli.getSession();
@@ -195,9 +224,17 @@ int main(void) {
     mux.handle("/boxes/{id}")
 
             .get([&](served::response &res, const served::request & req) {
-                std::string name;
+                std::string name = "";
+                std::string items = "";
+                std::string authKey = "";
+             
                 std::string header = req.header("cookie");
-                std::string authKey = getAuthKey(header);
+                auto cookies = parseCookies(header);
+                std::map<std::string, std::string>::iterator it;
+                it = cookies.find("authToken");
+                if (it != cookies.end()) {
+                    authKey = it->second;
+                }
                 std::string box_id_str = req.params["id"];
                 int box_id = stoi(box_id_str);
                 Session sess = cli.getSession();
@@ -206,11 +243,12 @@ int main(void) {
                     res << "{\"logout\": true}";
 
                 } else {
-                    //std::lock_guard<std::mutex> guard(l_box_items_array);
-                    // may not be worth global if one user is executing sql blocks all
-                    // or copy item then immediately release mutex lock not 
-                    // letting sql statement keep lock
-                    if (userItems[box_id] != "") {
+                    {
+                        // this is copy?  Safe ?  no lock if have to fetch with sql....
+                        std::lock_guard<std::mutex> guard(l_box_items_array);
+                        items = userItems[box_id];
+                    }
+                    if (items != "") {
                         std::string search = "\"user_id\":";
                                 search = search + std::to_string(user_id);;
                                 search = search + ",";
@@ -245,10 +283,10 @@ int main(void) {
                             buffer.seekp(-1, std::ios_base::end);
                         }
                         buffer << "]";
-                                userItems[box_id] = buffer.str();
+                                items = buffer.str();
                     }
                     if (user_id != 0) {
-                        res << userItems[box_id];
+                        res << items;
                     }
                 }
             });
@@ -256,9 +294,15 @@ int main(void) {
 
             .get([&](served::response &res, const served::request & req) {
                 std::string name;
+                std::string boxes;
+                std::string authKey = "";
                 std::string header = req.header("cookie");
-                std::string authKey = getAuthKey(header);
-
+                auto cookies = parseCookies(header);
+                std::map<std::string, std::string>::iterator it;
+                it = cookies.find("authToken");
+                if (it != cookies.end()) {
+                    authKey = it->second;
+                }
 
                 Session sess = cli.getSession();
                 int user_id = getUserIdFromAuthKey(cli, authKey);
@@ -266,11 +310,15 @@ int main(void) {
                     res << "{\"logout\": true}";
                     return;
                 }
-                 //std::lock_guard<std::mutex> guard(l_user_boxes_array);
-                if (userBoxes[user_id] != "") {
+                {
+                    // this is copy?  Safe ?  no lock if have to fetch with sql....
+                    std::lock_guard<std::mutex> guard(l_user_boxes_array);
+                    boxes = userBoxes[user_id];
+                }
+                if (boxes != "") {
                     // TODO:  needs to check if not "" if the user has access to the box in cache
                     // simalar to /boxes/{id} route
-                    
+
                 } else {
                     SqlResult myRows = sess.sql("SELECT id,user_id, name,weight, picture,created_at FROM boxes where user_id = ?")
                             .bind(user_id).execute();
@@ -291,16 +339,16 @@ int main(void) {
                         buffer.seekp(-1, std::ios_base::end);
                     }
                     buffer << "]";
-                            userBoxes[user_id] = buffer.str();
+                            boxes = buffer.str();
                 }
-                res << userBoxes[user_id];
+                res << boxes;
             });
 
     mux.handle("/login")
             .get([&](served::response &res, const served::request & req) {
                 std::stringstream buffer;
                 int user_id = 0;
-                std::string url = req.url().query();
+                //std::string url = req.url().query();
                 std::string username = req.query["username"]; //getQueryParam(url, "username");
                 std::string password = req.query["password"]; //getQueryParam(url, "password");
                 std::string authKey = "";
@@ -323,11 +371,13 @@ int main(void) {
                 } else {
                     // missing username and password message
                 }
-                buffer << "{";
-                buffer << "\"user_id\":" << "\"" << user_id << "\",";
-                buffer << "\"authToken\":" << "\"" << authKey << "\"";
-                buffer << "}";
-                res << buffer.str();
+                if (user_id != 0) {
+                    buffer << "{";
+                            buffer << "\"user_id\":" << "\"" << user_id << "\",";
+                            buffer << "\"authToken\":" << "\"" << authKey << "\"";
+                            buffer << "}";
+                            res << buffer.str();
+                }
             });
     mux.handle("/logout")
             .get([&](served::response &res, const served::request & req) {
